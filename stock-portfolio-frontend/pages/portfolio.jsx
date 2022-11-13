858e176fb523f8da4b91c9f0de25cd660630150e
Poh Anson <pohanson979@gmail.com>
2022-11-13 17:32:51 +0800

    Done up Portfolio Page

--- stock-portfolio-frontend/pages/portfolio.jsx
+++ stock-portfolio-frontend/pages/portfolio.jsx
@@ -1,3 +1,51 @@
+import { useEffect, useState } from "react";
+import LoadingPage from "../components/Loading";
+
 export default function PortfolioPage() {
-  return <h1>Portfolio</h1>;
+  let [portfolioData, setPortfolioData] = useState(null);
+  useEffect(() => {
+    fetch("/api/portfolio")
+      .then((r) => r.json())
+      .then(setPortfolioData);
+  }, []);
+  useEffect(() => console.log(portfolioData), [portfolioData]);
+  let table = (
+    <div className="overflow-auto">
+      <table>
+        <thead>
+          <tr className="border-b-2 border-b-gray-300">
+            <th>Stock Name/Code</th>
+            <th>Avg Price</th>
+            <th>Volume</th>
+            <th>Total Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {portfolioData == null ||
+            portfolioData.map((data) => <Row data={data} />)}
+        </tbody>
+      </table>
+    </div>
+  );
+  return (
+    <>
+      <h1>Portfolio</h1>
+      {portfolioData == null ? <LoadingPage /> : table}
+    </>
+  );
+}
+
+function Row({ data }) {
+  let { code, name, avg_price: price, volume, cost } = data;
+  return (
+    <tr key={code} className="odd:bg-gray-50 even:bg-white">
+      <td>
+        <p className="font-bold ">{name}</p>
+        <p className="text-sm text-gray-700">{code}</p>
+      </td>
+      <td>{price.toFixed(3)}</td>
+      <td>{volume}</td>
+      <td>{cost.toFixed(2)}</td>
+    </tr>
+  );
 }
