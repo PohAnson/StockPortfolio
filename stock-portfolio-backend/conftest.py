4b9e9bcdb9060b3412209d3c20b5a0797863620a
Poh Anson <pohanson979@gmail.com>
2023-05-11 20:43:39 +0800

    Added Test for User Api

--- stock-portfolio-backend/conftest.py
+++ stock-portfolio-backend/conftest.py
@@ -1,3 +1,72 @@
+import json
+import logging
+import os
+import subprocess
 import sys
+from time import sleep
+from pymongo import MongoClient
+
+import pytest
 
 sys.path.append("src")
+
+
+def pytest_configure():
+    pytest.server_url = "http://localhost:5000/api/v0/"
+
+
+MONGODB_PATH = "./tests/mongodb"
+logging.basicConfig(level=logging.ERROR)
+
+
+def remove_mongodb():
+    os.system(f"sudo rm -rf {MONGODB_PATH}")
+
+
+def start_mongo():
+    global mongod_pipe
+    if os.path.exists(MONGODB_PATH):
+        remove_mongodb()
+    os.mkdir(
+        MONGODB_PATH,
+    )
+    mongod_pipe = subprocess.Popen(
+        ["sudo", "mongod", "--dbpath", MONGODB_PATH], stdout=subprocess.DEVNULL
+    )
+
+
+def setup_db():
+    client = MongoClient()
+    with open("./tests/test_data/stock_data.json", "r") as f:
+        data = json.load(f)
+    client.data.stock.insert_many(data)
+
+
+def end_mongo():
+    mongod_pipe.terminate()
+
+
+def start_server():
+    global server_pipe
+    server_pipe = subprocess.Popen(
+        ["python", "src/main.py"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+
+def end_server():
+    server_pipe.terminate()
+
+
+def pytest_sessionstart(session):
+    start_mongo()
+    setup_db()
+    start_server()
+    sleep(2)
+
+
+def pytest_sessionfinish(session):
+    end_server()
+    end_mongo()
+    remove_mongodb()
---
+++ stock-portfolio-backend/tests/test_api_endpoint/test_user.py
@@ -0,0 +1,131 @@
+"""Test supported api method for user endpoint "/api/v0/user": GET, POST, PUT,
+and GET /logout
+"""
+
+import unittest
+
+import pytest
+import requests
+
+
+class UserApiTestcase(unittest.TestCase):
+    url = pytest.server_url + "user"
+    session_id_cookie = None
+
+    @classmethod
+    def setUpClass(cls):
+        # create new user and get a session id
+        response = cls.create_user(cls.url, "a", "pass", "A")
+        if response.status_code == 406:
+            # assumed that the username is already present
+            return
+        if response.status_code == 200:
+            cls.session_id_cookie = response.json().get("sassyid")
+
+    def check_login(self, session_id) -> bool:
+        response = requests.get(self.url, cookies={"sassyid": session_id})
+        return response.json().get("isLogin")
+
+    def login_user(self, username, password) -> requests.Response:
+        return requests.post(
+            self.url, json={"username": username, "password": password}
+        )
+
+    def logout_user(self, session_id) -> None:
+        requests.get(self.url + "/logout", cookies={"sassyid": session_id})
+
+    @staticmethod
+    def create_user(url, username, password, name) -> requests.Response:
+        return requests.put(
+            url,
+            json={"username": username, "password": password, "name": name},
+        )
+
+    ################################
+    # USER SESSION CHECK TEST CASE #
+    ################################
+
+    def test_checkSessionFail(self):
+        response = requests.get(self.url)
+        assert not response.json()["isLogin"]
+
+    def test_checkSessionPass(self):
+        assert self.check_login(self.session_id_cookie)
+
+    ###################
+    # LOGIN TEST CASE #
+    ###################
+
+    def test_loginUser(self):
+        username = "vno1408"
+        password = "as"
+        # Create user
+        response = self.create_user(self.url, username, password, "A")
+        session_id = response.json().get("sassyid")
+        self.logout_user(session_id)
+
+        response = self.login_user(username, password)
+        assert response.status_code == 200 and "sassyid" in response.json()
+
+    def test_failedLoginPassword(self):
+        response = self.create_user(self.url, "asdf", "a", "A")
+        self.logout_user(response.json().get("sassyid"))
+        response = self.login_user("asdf", "as")
+        assert response.status_code == 401 and "error" in response.json()
+
+    def test_loginUserInvalidUser(self):
+        response = self.login_user("casper", "ghost")
+        assert "error" in response.json() and response.status_code == 401
+
+    def test_loginUserInvalidFormat(self):
+        response = requests.post(
+            url=self.url, json={"user": "casper", "number": 1}
+        )
+        assert "error" in response.json() and response.status_code == 401
+
+    #########################
+    # USER LOGOUT TEST CASE #
+    #########################
+
+    def test_userLogout(self):
+        # Create new user
+        response = self.create_user(self.url, "logout6543", "as", "Unique")
+
+        if response.status_code != 200:
+            # Cannot create account properly
+            assert False, "Account not created"
+
+        session_id = response.json().get("sassyid")
+
+        # Check login before
+        assert self.check_login(session_id), "User is not login"
+
+        # Logout
+        self.logout_user(session_id)
+
+        # Check that user is logout
+        assert not self.check_login(
+            session_id
+        ), "User is still login when they should be logout"
+
+    ##########################
+    # USER SIGN UP TEST CASE #
+    ##########################
+
+    def test_userSignupPass(self):
+        response = self.create_user(self.url, "unique3421", "as", "Unique")
+
+        assert response.status_code == 200 and "sassyid" in response.json()
+
+    def test_userSignUpDuplicateUsernameError(self):
+        self.create_user(self.url, "commonName", "as", "Unique")
+
+        response = self.create_user(self.url, "commonName", "asd", "diffName")
+        assert response.status_code == 406
+        assert "error" in response.json()
+
+    def test_userSignUpDifferentUsernameSameParticular(self):
+        self.create_user(self.url, "uniqueName", "as", "Unique")
+        response = self.create_user(self.url, "uniqueName21", "as", "Unique")
+
+        assert response.status_code == 200 and "sassyid" in response.json()
